e4afc3d76397166102e095d2be0da7ee22c03e9e
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-31 14:51:01 -0300

    task 1 v2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = NULL, *aux;
 
+	if (parent == NULL)
+		return (NULL);
 	newnode = binary_tree_node(parent, value);
 
 	aux = parent->left;
