2a6b87ed94f4d1c1477ecaba21ddaca7f45f6318
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-01 11:49:51 -0300

    aux removed

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,8 +10,7 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = NULL, *aux;
-
+	binary_tree_t *newnode = NULL;
 	if (parent == NULL)
 		return (NULL);
 
