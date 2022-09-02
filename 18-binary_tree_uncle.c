6e15272d38fb285725d7ab2ed694f06edb3289fd
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-02 15:53:49 -0300

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ *
+ * @node: pointer to the node
+ * Return: pointer to the uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	return (binary_tree_sibling(node->parent));
+}
+
+
+/**
+ * binary_tree_sibling - find the sibiling of a node
+ * @node: node
+ * Return: pointer to the sibiling of @node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	int currentNodePosition = 0; /*0 is left, 1 is right*/
+
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left != node)
+		currentNodePosition = 1;
+
+	if (currentNodePosition == 1)
+		if (node->parent->left)
+			return (node->parent->left);
+
+	if (currentNodePosition == 0)
+		if (node->parent->right)
+			return (node->parent->right);
+
+	return (NULL);
+}
