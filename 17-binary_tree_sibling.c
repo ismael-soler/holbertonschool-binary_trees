9ef1065208b0eaf7ef9f08fb347a9eecf5f50f16
ismael-soler <ismaelsolerferreira@gmail.com>
2022-09-02 15:45:59 -0300

    task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibiling - find the sibilg of a node
+ * @node: node
+ * Return: pointer to the sibiling of @node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	int currentNodePosition = 0; /*0 is left, 1 is right*/
+
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left != node)
+	{
+		currentNodePosition = 1;
+	}
+
+	if (currentNodePosition == 1)
+	{
+		if (node->parent->left)
+		{
+			return (node->parent->left);
+		}
+	}
+	if (currentNodePosition == 0)
+	{
+		if (node->parent->right)
+		{
+			return (node->parent->right);
+		}
+	}
+	return (NULL);
+}
